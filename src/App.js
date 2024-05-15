6dd4d8b00b2ae8f2be5636f2d1aa961fcd1e00c1
SFelbeck <stefanfelbeck@gmail.com>
2024-05-15 00:12:04 -0700

    trigger

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import PortfolioContainer from "./components/PortfolioContainer";
+// import Header from "./components/Header";
+// import Footer from "./components/Footer";
+
+//header and footer?
+function App() {
+  return (
+    // <Header />
+    <PortfolioContainer />
+    // <Footer />
+  )
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function header() {
+    <header class="header">
+        <h1>Stefan Felbeck</h1>
+        
+        {/* <nav>
+            <a href="#about-me">About Me</a>
+            <a href="#work">Work</a>
+            <a href="#contact-me">Contact Me</a>
+            <a href="./public/assets/resume.pdf">Resume</a>
+        </nav> */}
+    </header>
+}
+
+// export default header;
